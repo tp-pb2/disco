d7f5138c61030aec9ffbd12e6b8e1dbd50f3b63e
basco1979 <el_basco@hotmail.com>
2019-04-22 21:26:48 -0300

    disco1

---
+++ Disco.java
@@ -0,0 +1,47 @@
+package practicaFinal;
+
+public class Disco {
+	
+	private Double radioInterior;
+	private Double radioExterior;
+	private final static Double PI=3.14;
+	
+	public Disco(Double radioInterior, Double radioExterior) {
+		this.radioExterior= radioExterior;
+		this.radioInterior = radioInterior;
+	}
+	
+	
+	public Double calcularPerimetroInterior() {
+		return 2*PI*radioInterior; 
+	}
+	
+	public Double calcularPerimetroExterior() {
+		return 2*PI*radioExterior; 
+	}
+	
+	public Double calcularSuperficie() {
+		return (PI*radioExterior*radioExterior)- (PI*radioInterior*radioInterior);
+	}
+
+
+	public Double getRadioInterior() {
+		return radioInterior;
+	}
+
+
+	public void setRadioInterior(Double radioInterior) {
+		this.radioInterior = radioInterior;
+	}
+
+
+	public Double getRadioExterior() {
+		return radioExterior;
+	}
+
+
+	public void setRadioExterior(Double radioExterior) {
+		this.radioExterior = radioExterior;
+	}
+	
+}
